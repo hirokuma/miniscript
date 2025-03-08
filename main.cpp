0693fd2bf355d5dce29c281976f40ad724b9f209
hirokuma <hiro9ma@gmail.com>
2025-03-08 11:37:23 +0900

    output format

--- main.cpp
+++ main.cpp
@@ -19,11 +19,51 @@ static bool run(std::string&& line, int64_t count) {
     if (Compile(Expand(line), ret, avgcost)) {
         auto str = ret->ToString(COMPILER_CTX);
         assert(str);
-        printf("X %17.10f %5i %s %s\n", ret->ScriptSize() + avgcost, (int)ret->ScriptSize(), Abbreviate(std::move(*str)).c_str(), line.c_str());
+        auto das = ret->ToScript(COMPILER_CTX);
+        printf("<<Spending cost>>\nscript_size=%5i\ninput_size=%17.10f\ntotal_cost=%17.10f\n\n"
+            "<<miniscript output>>\n%s\n\n"
+            "<<Resulting script structure>>\n%s\n",
+                (int)ret->ScriptSize(),
+                avgcost,
+                ret->ScriptSize() + avgcost,
+                Abbreviate(std::move(*str)).c_str(),
+                Disassemble(das).c_str());
     } else if ((ret = miniscript::FromString(Expand(line), COMPILER_CTX))) {
         auto ms = ret->ToString(COMPILER_CTX);
         assert(ms);
-        printf("%7li scriptlen=%i maxops=%i type=%s safe=%s nonmal=%s dissat=%s input=%s output=%s timelock_mix=%s miniscript=%s\n", (long)count, (int)ret->ScriptSize(), (int)ret->GetOps(), ret->GetType() << "B"_mst ? "B" : ret->GetType() << "V"_mst ? "V" : ret->GetType() << "W"_mst ? "W" : ret->GetType() << "K"_mst ? "K" : "(invalid)", ret->GetType() << "s"_mst ? "yes" : "no", ret->GetType() << "m"_mst ? "yes" : "no", ret->GetType() << "f"_mst ? "no" : ret->GetType() << "e"_mst ? "unique" : ret->GetType() << "d"_mst ? "yes" : "unknown", ret->GetType() << "z"_mst ? "0" : ret->GetType() << "o"_mst ? (ret->GetType() << "n"_mst ? "1n" : "1") : ret->GetType() << "n"_mst ? "n" : "-", ret->GetType() << "u"_mst ? "1" : "nonzero", ret->GetType() << "k"_mst ? "no": "yes", Abbreviate(*ms).c_str());
+        printf("count=%li\n"
+            "scriptlen=%i\n"
+            "maxops=%i\n"
+            "type=%s\n"
+            "safe=%s\n"
+            "nonmal=%s\n"
+            "dissat=%s\n"
+            "input=%s\n"
+            "output=%s\n"
+            "timelock_mix=%s\n"
+            "miniscript=%s\n",
+                (long)count,
+                (int)ret->ScriptSize(),
+                (int)ret->GetOps(),
+                ret->GetType()
+                    << "B"_mst ? "B" : ret->GetType()
+                    << "V"_mst ? "V" : ret->GetType()
+                    << "W"_mst ? "W" : ret->GetType()
+                    << "K"_mst ? "K" : "(invalid)",
+                ret->GetType() << "s"_mst ? "yes" : "no",
+                ret->GetType() << "m"_mst ? "yes" : "no",
+                ret->GetType()
+                    << "f"_mst ? "no" : ret->GetType()
+                    << "e"_mst ? "unique" : ret->GetType()
+                    << "d"_mst ? "yes" : "unknown",
+                ret->GetType()
+                    << "z"_mst ? "0" : ret->GetType()
+                    << "o"_mst ? (ret->GetType()
+                    << "n"_mst ? "1n" : "1") : ret->GetType()
+                    << "n"_mst ? "n" : "-",
+                ret->GetType() << "u"_mst ? "1" : "nonzero",
+                ret->GetType() << "k"_mst ? "no": "yes",
+                Abbreviate(*ms).c_str());
     } else {
         printf("Failed to parse as policy or miniscript '%s'\n", line.c_str());
     }
